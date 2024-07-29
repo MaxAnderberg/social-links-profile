a43bee2fd5d5696fcb5d28b7ff2e5929746053dc
Max <max@DESKTOP-M63F2LC>
2024-07-29 22:00:19 +0200

    init vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ProfileImage />
+</template>
+
+<script>
+import ProfileImage from './components/ProfileImage'
+
+export default {
+  name: 'App',
+  components: {
+    ProfileImage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="component">
+        <h1>{{ title }}</h1>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ProfileImage',
+        data() {
+            return {
+                title: 'Hello Vue',
+                message: 'This is a basic vue component'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .component {
+        background-color: aqua;
+    }
+</style>
