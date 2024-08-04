7b85ddf9ef155921461fe4957ba66cfa268a9f63
Max <max@DESKTOP-M63F2LC>
2024-08-04 22:25:19 +0200

    updated userinfo

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>{{ Name }}</h1>
+        <p class="location">{{ Location }}</p>
+        <p>{{ Description }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UserInfo',
+        data() {
+            return {
+                HelloWorld: 'Hello There World',
+                Name: 'Jessica Randall',
+                Location: 'London, United Kingdom',
+                Description: 'Front-end developer and avid reader'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .name {
+        font-weight: bold;
+    }
+    .location {
+        color:  var(--color-green)
+    }
+</style>
