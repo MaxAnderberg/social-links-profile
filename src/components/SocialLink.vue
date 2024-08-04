01be89a7a62bc4f07767701c7a0b4606218efe9f
Max <max@DESKTOP-M63F2LC>
2024-08-04 22:25:01 +0200

    added sociallink component

---
+++ src/components/SocialLink.vue
@@ -0,0 +1,27 @@
+<template>
+    <a href="link" class='social-link'>{{ name }}</a>
+</template>
+
+<script>
+export default {
+    name: 'SocialLink',
+    props: {
+        name : {
+            type: String,
+            required: true
+        },
+        link: {
+            type: String,
+            required: true
+        } 
+    }
+}
+
+</script>
+
+<style scoped>
+.social-link {
+    color: red;
+    background-color: var(--color-grey-700);
+}
+</style>
