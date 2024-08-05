c669755a60f198e4f6901dd9b7dc1ffe7c357221
Max <max@DESKTOP-M63F2LC>
2024-08-05 21:33:12 +0200

    added correct links and name

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <ProfileImage />
-  <UserInfo/>
-  <div class='links-container'>
-    <SocialLink link="https://google.se" name="Google"/>
-    <SocialLink link="https://google.se" name="Google"/>
-    <SocialLink link="https://google.se" name="Google"/>
-    <SocialLink link="https://google.se" name="Google"/>
-  </div>
+  <main class="main">
+    <ProfileImage />
+    <UserInfo/>
+    <div class='links-container'>
+      <SocialLink link="https://Github.com" name="GitHub"/>
+      <SocialLink link="https://frontendmentor.io/" name="Frontend Mentor"/>
+      <SocialLink link="https://linkedin.com" name="LinkedIn"/>
+      <SocialLink link="https://twitter.com" name="Twitter"/>
+      <SocialLink link="https://instagram.com" name="Instagram"/>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -26,16 +29,29 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #FFFFFF;
-  margin-top: 60px;
 }
 .links-container {
   display: flex;
   flex-direction: column;
   gap: 12px;
+
+}
+.main {
+  display: flex;
+  flex-direction: column;
+  margin-top: 60px;
+  max-width: 40%;
+  justify-content: center;
+  justify-items: center;
+  background-color: var(--color-grey-800);
+  padding: 40px;
+  border-radius: 8px;
 }
 </style>
